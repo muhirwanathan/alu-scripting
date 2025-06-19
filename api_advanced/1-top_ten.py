e0504ec44f4cf867c0db9fe7d79bc71784b74a27
muhirwanathan <n.muhirwa@alustudent.com>
2025-06-19 13:19:36 -0700

    111--11

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,22 +5,32 @@ import requests
 
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts listed for a subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    if not subreddit or not isinstance(subreddit, str):
+        print("None")
+        return
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'MyRedditClient/1.0'}
+    params = {'limit': 10}
+
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+
+    if response.status_code != 200:
+        print("None")
+        return
 
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code != 200:
+        posts = response.json().get("data", {}).get("children", [])
+        if not posts:
             print("None")
             return
 
-        data = response.json().get("data", {}).get("children", [])
-        if not data:
-            print("None")
-            return
-
-        for post in data:
-            print(post.get("data", {}).get("title"))
-
+        for post in posts:
+            title = post.get("data", {}).get("title")
+            if title:
+                print(title)
+            else:
+                print("None")
     except Exception:
         print("None")
+
