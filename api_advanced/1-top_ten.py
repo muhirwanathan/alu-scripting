1cbad1a91e1348bbbe5c9b4ac6953e871014c41c
muhirwanathan <n.muhirwa@alustudent.com>
2025-06-19 13:16:57 -0700

    111

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-""" 1-top_ten.py """
+"""1-top_ten.py"""
 import requests
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    """Prints the titles of the first 10 hot posts listed for a subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        print(None)
-        return
-    posts = response.json()['data']['children']
-    for post in posts:
-        print(post['data']['title'])
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            print("None")
+            return
+
+        data = response.json().get("data", {}).get("children", [])
+        if not data:
+            print("None")
+            return
+
+        for post in data:
+            print(post.get("data", {}).get("title"))
+
+    except Exception:
+        print("None")
